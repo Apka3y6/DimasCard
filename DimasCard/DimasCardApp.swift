a25029ab7aeea8f6ecb88d9ce29fdcea24184ec9
Apka3y6 <vilgaukd@gmail.com>
2024-01-12 05:01:51 +0500

    Initial Commit

---
+++ DimasCard/DimasCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DimasCardApp.swift
+//  DimasCard
+//
+//  Created by Dmitrii Vilgauk on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DimasCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
